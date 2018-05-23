5d4ddf949d4fe497841e09b829ea1e2405972838
Alexandr Snitkovskiy <alexandr.snitkovskiy@rhinocodes.com>
2018-05-23 09:54:38 +0300

    Added main map requests and extended test data.

--- src/Map.js
+++ src/Map.js
@@ -3,24 +3,60 @@ import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-map
 
 const testData = [
     {
-        "lat": 53.817680,
-        "lng": -1.537657
+        "lat": 50.303279,
+        "lng": 29.177015,
     },
     {
-        "lat": 53.790123,
-        "lng": -1.53243
+        "lat": 50.625160,
+        "lng": 31.258765,
     },
     {
-        "lat": 53.756745,
-        "lng": -1.5309087
+        "lat": 50.890463,
+        "lng": 33.016186,
     },
     {
-        "lat": 53.6474675,
-        "lng": -1.49564554
+        "lat": 46.967140,
+        "lng": 31.968177,
     },
     {
-        "lat": 53.69123456,
-        "lng": -1.6545466
+        "lat": 46.481235,
+        "lng": 30.582511,
+    },
+    {
+        "lat": 47.847250,
+        "lng": 35.303733,
+    },
+    {
+        "lat": 51.064592,
+        "lng": 32.256909,
+    },
+    {
+        "lat": 49.776881,
+        "lng": 23.960680,
+    },
+    {
+        "lat": 50.663408,
+        "lng": 25.764981,
+    },
+    {
+        "lat": 51.452022,
+        "lng": 24.838470,
+    },
+    {
+        "lat": 51.552539,
+        "lng": 25.852206,
+    },
+    {
+        "lat": 50.663408,
+        "lng": 25.764981,
+    },
+    {
+        "lat": 49.312515,
+        "lng": 30.707931,
+    },
+    {
+        "lat": 50.538415,
+        "lng": 29.478510,
     }
 ];
 
@@ -34,8 +70,8 @@ const Map = withScriptjs(
 
         return (
             <GoogleMap
-                defaultZoom={4}
-                defaultCenter={{ lat: 40.022, lng: -98.132 }}
+                defaultZoom={6}
+                defaultCenter={{ lat: 48.868554, lng: 32.053234 }}
             >
                 {locationData.length > 0 && locationData.map(item => 
                     <Marker key={item.lat} position={item} />
@@ -73,7 +109,7 @@ class WrappedMap extends PureComponent {
                     googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyD_nHJXh-xRpNrLlkVXhPZHOH3gHyjQ5OA&v=3.exp&libraries=geometry,drawing,places"
                     
                     loadingElement={<div style={{ height: `100%` }} />}
-                    containerElement={<div style={{ height: `500px` }} />}
+                    containerElement={<div style={{ height: `520px` }} />}
                     mapElement={<div style={{ height: `100%` }} />}
                 />
             </Fragment>
---
+++ src/requests.js
@@ -0,0 +1,91 @@
+const getGeoCode = (options) => new Promise(resolve => {
+    const geocoder = new window.google.maps.Geocoder();
+    geocoder.geocode(options.params, (results, status) => {
+        if (status === 'OK') {
+            let city = 'unknown city';
+            let state = 'unknown state';
+            let zipCode = 'unknown zipCode';
+            let country = 'unknown country';
+
+            for (let i = 0; i < results[0].address_components.length; i++) {
+                const component = results[0].address_components[i];
+                switch (component.types[0]) {
+                    case 'locality':
+                        city = component.long_name;
+                        break;
+                    case 'administrative_area_level_1':
+                        state = component.short_name;
+                        break;
+                    case 'postal_code':
+                        zipCode = component.short_name;
+                        break;
+                    case 'country':
+                        country = component.short_name;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            resolve({
+                ...options.payload,
+                formatted_address: `${city}, ${state} ${zipCode}, ${country}`,
+                lat: results[0].geometry.location.lat(),
+                lng: results[0].geometry.location.lng(),
+            });
+        } else if (status === 'OVER_QUERY_LIMIT') {
+            console.error("GOOGLE API ERROR -> OVER QUERY LIMIT");
+            refreshApiToken();
+        } else {
+            console.warn(`Geocode was not successful for the following reason: ${status}`);
+            resolve([]);
+        }
+    });
+});
+
+export function getGeoCodeData(params) {
+    const processRequests = async () => {
+        const result = [];
+        // eslint-disable-next-line
+        for (const geoDataParams of params) {
+            // eslint-disable-next-line
+            await delay(1000);
+            // eslint-disable-next-line
+            const geoData = await getGeoCode(geoDataParams);
+            result.push(geoData);
+        }
+        return result;
+    };
+    return processRequests();
+}
+
+export function requestDirections(points) {
+    let tryAgain = true;
+
+    const fetchData = (directionsService, resolve) => {
+        directionsService.route({
+            origin: points.origin,
+            destination: points.destination,
+            waypoints: points.waypoints,
+            optimizeWaypoints: false,
+            travelMode: 'DRIVING',
+        }, (response, status) => {
+            if (status === 'OVER_QUERY_LIMIT' && tryAgain) {
+                setTimeout(() => {
+                    fetchData(directionsService, resolve);
+                    tryAgain = false;
+                }, 500);
+            } else if (status === 'OVER_QUERY_LIMIT') {
+                console.error("GOOGLE API ERROR -> OVER QUERY LIMIT");
+                refreshApiToken();
+            } else {
+                resolve({data: response, status});
+            }
+        });
+    };
+
+    return new Promise((resolve) => {
+        const directionsService = new window.google.maps.DirectionsService();
+        fetchData(directionsService, resolve);
+    });
+}
