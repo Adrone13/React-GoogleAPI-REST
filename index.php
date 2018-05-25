f2bbab6c4b401188021e9e66a9e47169a185218b
Alexandr Snitkovskiy <alexandr.snitkovskiy@rhinocodes.com>
2018-05-25 16:26:34 +0300

    Remove unused code from index.php.

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php
 
-require_once("DB.php");
+require_once('DB.php');
 
 $host = 'localhost';
-// $db_name = 'rc_expedite';
 $db_name = 'my-project';
 $username = 'root';
 $password = '8246';
@@ -12,28 +11,14 @@ $db = new DB($host, $db_name, $username, $password);
 
 if ($_SERVER['REQUEST_METHOD'] == "GET") {
 
-    if ($_GET['url'] == "users") {
-        $usersArray = array_values($db->query("SELECT * FROM user"));
-
-        header('Content-type: application/json');
-        header('Access-Control-Allow-Origin: *');
-
-        echo json_encode($usersArray);
-        http_response_code(200);
-    }
-
     if ($_GET['url'] == "coordinates") {
         $coordinatesArray = array_values($db->query("SELECT * FROM coordinates"));
 
         header('Content-type: application/json');
-        header('Access-Control-Allow-Origin: *');
         
         echo json_encode($coordinatesArray);
         http_response_code(200);
-    }
-
-    if ($_GET['url'] == "loads") {
-        // $coordinatesArray = array_values($db->query("SELECT * FROM loads RIGHT JOIN coordinates ON loads.id = coordinates.object_id"));
+    } else if ($_GET['url'] == "loads") {
         $loads = array_values($db->query("SELECT * FROM loads"));
         $coordinatesArray = array_values($db->query("SELECT * FROM coordinates"));
 
@@ -50,13 +35,9 @@ if ($_SERVER['REQUEST_METHOD'] == "GET") {
         header('Content-type: application/json');
         echo json_encode($loads);
         http_response_code(200);
-    }
-
-} else if ($_SERVER['REQUEST_METHOD'] == "POST") {
-    
-    if ($_GET['url'] == "users") {
-        $postBody = file_get_contents("php://input");
-        echo $postBody;
+    } else {
+        http_response_code(404);
+        echo "Not Found";
     }
 } else {
     http_response_code(405);
